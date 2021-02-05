b998a4adbe6022e0f69c9603c19192383d561aec
Duco Lindhout <duco.lindhout@gmail.com>
2021-02-05 12:13:57 +0100

    Update readme and small refactor

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# complex
+# Complex
 Complex number plotter using domain coloring
+
+## Usage
+ 1. `git clone https://github.com/Ducolnd/complex`
+ 2. `cd complex`
+ 3. Install python 3 and virtualenv
+ 4. `virtualenv venv`
+ 5. `source venv/bin/activate`
+ 6. `pip install -r requirements.txt`
+ 7. Open complex.py and edit `function` function
+ 8. Run by typing `python complex.py` and close by pressing ECS
+
+## Sources
+[algorithm-archive.org](https://www.algorithm-archive.org/contents/domain_coloring/domain_coloring.html)
+[dynamicmath.xyz](https://www.dynamicmath.xyz/domain-coloring/#basic)
+[wikipedia.org](https://en.wikipedia.org/wiki/Domain_coloring)
--- complex.py
+++ complex.py
@@ -8,15 +8,18 @@ size = 400
 res = np.linspace(-2, 2, size * 2 + 1)
 ims = np.linspace(-2, 2, size * 2 + 1)
 
-
 nums = np.zeros((len(res) - 1, len(ims) - 1), dtype=np.complex_)
 
 for i in range(-size, size):
     for j in range(-size , size):
         nums[i+size, j+size] = complex(res[i+size], ims[j+size])
 
+
+# Edit this
 def function(complex_num):
     return complex_num ** 2
+
+
 
 nums = function(nums)
 
@@ -26,7 +29,6 @@ magnitude = np.abs(nums)
 
 H = np.interp(phase, (-180, 180), (0,  360))
 S = 0.7 + (1 / 3) * (np.log(magnitude) / np.log(1.6) - np.floor(np.log(magnitude) / np.log(1.6)))   # alternatively S = 0.5 + 0.5 * (magnitude - np.floor(magnitude))
-
 V = ((np.abs(np.sin(math.pi * nums.real)) ** 0.1) * (np.abs(np.sin(math.pi * nums.imag)) ** 0.1)) * 255 # alternatively V = np.full(phase.shape, 255, dtype="float32")
 
 
